a1c904e7f2a3ece5094cd5e68c4e366e4c0a4b5f
Rick Henry <rickhenry@rickhenry.dev>
2023-06-07 20:35:33 -0400

    add stopwatchjs
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

---
+++ staticfiles/stopwatch.js
@@ -0,0 +1,54 @@
+document.addEventListener('alpine:init', function () {
+  Alpine.store('stopwatch', {
+    secondsPassed: 0,
+    started: false,
+    ticker: null,
+    ready: false,
+
+    start() {
+      this.ticker = setInterval(() => {
+        this.tick();
+      }, 1000);
+      this.started = true;
+    },
+
+    pause() {
+      clearInterval(this.ticker);
+      this.ticker = null;
+    },
+
+    tick() {
+      this.secondsPassed +=1;
+    },
+
+    reset() {
+      this.init();
+    },
+
+    init() {
+      this.secondsPassed = 0;
+      if (this.ticker) {
+        clearInterval(this.ticker);
+        this.ticker = null;
+      }
+      this.ready = true;
+      this.started = false;
+    },
+
+    get mins() {
+      const m =  Math.floor(this.secondsPassed / 60);
+      return m.toString().padStart(2, "0");
+    },
+
+    get secs() {
+      const s = Math.floor(this.secondsPassed % 60);
+      return s.toString().padStart(2, "0");
+    },
+
+    get running() {
+      return this.ticker != null;
+    },
+  });
+
+  Alpine.store('stopwatch').init();
+});
